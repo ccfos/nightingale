a1c47b7ca35574cb6d05fefd904aaf972202966a
matao3754 <375477717@qq.com>
2020-06-07 09:36:58 +0800

    添加endpoint屏蔽,并且修改页面对应必填项,去除mcache不必要的锁
    * 修改：     go.mod
    	修改：     go.sum
    	重命名：   pub/index-c6eeb66b35b8fd41c6bf.css -> pub/index-835a6df5e01917561f12.css
    	新文件：   pub/index-835a6df5e01917561f12.js
    	新文件：   pub/index-835a6df5e01917561f12.js.map
    	删除：     pub/index-c6eeb66b35b8fd41c6bf.js
    	删除：     pub/index-c6eeb66b35b8fd41c6bf.js.map
    	修改：     pub/index.html
    	修改：     src/model/user.go
    	修改：     src/modules/monapi/cron/mask.go
    	修改：     src/modules/monapi/mcache/mask.go
    	修改：     web/package-lock.json
    	修改：     web/src/pages/Monitor/Silence/CustomForm.tsx
    
    * Update mask.go
    
    Co-authored-by: 马涛 <matao@staff.sina.com.cn>

--- src/modules/monapi/cron/mask.go
+++ src/modules/monapi/cron/mask.go
@@ -74,9 +74,13 @@ func IsMaskEvent(event *model.Event) bool {
 			eventTagsList = append(eventTagsList, fmt.Sprintf("%s=%s", strings.TrimSpace(k), strings.TrimSpace(v)))
 		}
 		key := eventMetric + "#" + event.Endpoint
-		maskTagsList, exists := mcache.MaskCache.GetByKey(key)
+		endpointKey := "#" + event.Endpoint
+		maskTagsList, exists := mcache.MaskCache.GetByKey(endpointKey)
 		if !exists {
-			continue
+			maskTagsList, exists = mcache.MaskCache.GetByKey(key)
+			if !exists {
+				continue
+			}
 		}
 
 		for i := 0; i < len(maskTagsList); i++ {
