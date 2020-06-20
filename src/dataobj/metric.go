7d5d791376e59879f515584d802e56ae446034c1
Ulric Qin <ulric.qin@gmail.com>
2020-06-20 22:59:47 +0800

    support SUBTRACT

--- src/dataobj/metric.go
+++ src/dataobj/metric.go
@@ -87,7 +87,7 @@ func (m *MetricValue) CheckValidity(now int64) (err error) {
 		m.CounterType = GAUGE
 	}
 
-	if m.CounterType != GAUGE && m.CounterType != COUNTER {
+	if m.CounterType != GAUGE && m.CounterType != COUNTER && m.CounterType != SUBTRACT {
 		err = fmt.Errorf("wrong counter type")
 		return
 	}
