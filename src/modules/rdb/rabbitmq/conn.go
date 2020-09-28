fe81c6cad71c140e3f4e48f7cf1470511a548019
alick-liming <whandlm@163.com>
2020-09-28 17:20:31 +0800

    配置调整，连接检查 (#314)
    * 1.rabbitmq 配置文件 2.连接检查
    
    * 代码调整
    
    * 启动协程
    
    * rabbitmq 连接检查代码调优
    
    * rabbitmq 连接检查代码调优
    
    Co-authored-by: alickliming <alickliming@didi.global.com>

--- src/modules/rdb/rabbitmq/conn.go
+++ src/modules/rdb/rabbitmq/conn.go
@@ -1,9 +1,11 @@
 package rabbitmq
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/streadway/amqp"
+	"github.com/toolkits/pkg/logger"
 )
 
 var (
@@ -16,6 +18,54 @@ func Init(url string) {
 	conn, err = amqp.Dial(url)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+// ping 测试rabbitmq连接是否正常
+func ping() (err error) {
+	if conn == nil {
+		return fmt.Errorf("conn is nil")
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		logger.Error(err)
+		close()
+		return err
+	}
+
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare("ping.ping", "topic", false, true, false, true, nil)
+	if err != nil {
+		logger.Error(err)
+		return err
+	}
+
+	msgContent := "ping.ping"
+	err = ch.Publish("ping.ping", "ping.ping", false, false, amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte(msgContent),
+	})
+	if err != nil {
+		logger.Error(err)
+		return err
+	}
+	err = ch.ExchangeDelete("ping.ping", false, false)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	return err
+}
+
+func close() {
+	if conn != nil {
+		err := conn.Close()
+		if err != nil {
+			logger.Error(err)
+		}
+		conn = nil
 	}
 }
 
