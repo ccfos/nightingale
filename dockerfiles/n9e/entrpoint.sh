17a3b8f78e0a85f5fc73ee9b0e2276cc515c2a8f
yimeng <1982337+yimeng@users.noreply.github.com>
2020-09-29 09:41:30 +0800

    1 修复agent缺失命令 2 二次构建二进制 3 mysql启动等待

--- dockerfiles/n9e/entrpoint.sh
+++ dockerfiles/n9e/entrpoint.sh
@@ -2,13 +2,20 @@
 sed -i 's/127.0.0.1/mysql/g' etc/mysql.yml
 sed -i 's/addr: 127.0.0.1:6379/addr: redis:6379/g' etc/rdb.yml
 
-mysql -hmysql -uroot -p1234 < sql/n9e_ams.sql
-mysql -hmysql -uroot -p1234 < sql/n9e_hbs.sql
-mysql -hmysql -uroot -p1234 < sql/n9e_job.sql
-mysql -hmysql -uroot -p1234 < sql/n9e_mon.sql
-mysql -hmysql -uroot -p1234 < sql/n9e_rdb.sql
-mysql -hmysql -uroot -p1234 < sql/n9e_rdb.sql
-mysql -hmysql -uroot -p1234 < sql/n9e_rdb.sql
+mysqlRootPassword=1234
+
+until mysql -hmysql -u root -p$mysqlRootPassword  -e ";" ; do
+    echo "Can't connect mysql, retry"
+    sleep 5
+done
+
+mysql -hmysql -uroot -p$mysqlRootPassword < sql/n9e_ams.sql
+mysql -hmysql -uroot -p$mysqlRootPassword < sql/n9e_hbs.sql
+mysql -hmysql -uroot -p$mysqlRootPassword < sql/n9e_job.sql
+mysql -hmysql -uroot -p$mysqlRootPassword < sql/n9e_mon.sql
+mysql -hmysql -uroot -p$mysqlRootPassword < sql/n9e_rdb.sql
+mysql -hmysql -uroot -p$mysqlRootPassword < sql/n9e_rdb.sql
+mysql -hmysql -uroot -p$mysqlRootPassword < sql/n9e_rdb.sql
 
 ./control start all
 sleep infinity
