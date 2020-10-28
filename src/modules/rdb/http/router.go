c6b5a5b400a78b327efdbedd7e2af810e3f6eb01
yubo <steveyubo@didichuxing.com>
2020-10-29 07:03:57 +0800

    feature: support reset password by sms code (#365)
    * add logout v2 for sso
    
    * support sms-code login
    
    * use db instead of memory cache for login code
    
    * feature: support reset password by sms code

--- src/modules/rdb/http/router.go
+++ src/modules/rdb/http/router.go
@@ -25,6 +25,9 @@ func Config(r *gin.Engine) {
 		notLogin.GET("/auth/v2/authorize", authAuthorizeV2)
 		notLogin.GET("/auth/v2/callback", authCallbackV2)
 		notLogin.GET("/auth/v2/logout", logoutV2)
+
+		notLogin.POST("/auth/send-rst-code-by-sms", sendRstCodeBySms)
+		notLogin.POST("/auth/rst-password", rstPassword)
 	}
 
 	hbs := r.Group("/api/hbs")
