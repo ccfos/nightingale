bddd93cd80966ab60b69b5ad957d20a76d06f082
710leo <710leo@gmail.com>
2020-11-17 18:32:52 +0800

    udpate rdb sql

--- sql/n9e_rdb.sql
+++ sql/n9e_rdb.sql
@@ -17,6 +17,8 @@ CREATE TABLE `user`
     `portrait`     varchar(2048) not null default '',
     `intro`        varchar(2048) not null default '',
     `organization` varchar(255) not null default '',
+    `typ`          tinyint(1)   not null default 1 comment '0: temporary account; 1: long-term account'
+    `status`       tinyint(1)   not null default 1 comment '0: disabled; 1: active 2: inactive'
     `is_root`      tinyint(1)   not null,
     `leader_id`    int unsigned not null default 0,
     `leader_name`  varchar(32)  not null default '',
