2e9b2364061f2ef124c65dfb7eab63a5cff21ff7
UlricQin <ulric.qin@gmail.com>
2021-01-22 11:29:49 +0800

    3.5.0 release

--- sql/n9e_mon-patch.sql
+++ sql/n9e_mon-patch.sql
@@ -1,7 +1,1 @@
-set names utf8;
-use n9e_mon;
 
-
-alter table collect_rule change `last_updator` `updater` varchar(64) NOT NULL DEFAULT '' COMMENT 'updater';
-alter table collect_rule add `created_at` bigint NOT NULL DEFAULT 0;
-alter table collect_rule add `updated_at` bigint NOT NULL DEFAULT 0;
