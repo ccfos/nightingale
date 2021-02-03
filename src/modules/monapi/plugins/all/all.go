3fdd61edfc482db6798fb25f10b94b0b0584732b
sun763625521 <763625521@qq.com>
2021-02-03 15:02:48 +0800

    新增rabbitmq、haproxy组件采集 (#575)
    * add
    
    * add prober plugin for rabbitmq
    
    * add prober plugin for haproxy
    
    Co-authored-by: root <root@localhost.localdomain>
    Co-authored-by: UlricQin <ulric.qin@gmail.com>

--- src/modules/monapi/plugins/all/all.go
+++ src/modules/monapi/plugins/all/all.go
@@ -11,6 +11,8 @@ import (
 	_ "github.com/didi/nightingale/src/modules/monapi/plugins/redis"
 	_ "github.com/didi/nightingale/src/modules/monapi/plugins/nginx"
 	_ "github.com/didi/nightingale/src/modules/monapi/plugins/elasticsearch"
+	_ "github.com/didi/nightingale/src/modules/monapi/plugins/rabbitmq"
+	_ "github.com/didi/nightingale/src/modules/monapi/plugins/haproxy"
 	_ "github.com/didi/nightingale/src/modules/monapi/plugins/tengine"
 	_ "github.com/didi/nightingale/src/modules/monapi/plugins/zookeeper"
 
