7f92e921b4ccf6f92e397c32e7c2d6c6789bba75
xtan <38320121+tanxiao1990@users.noreply.github.com>
2022-05-30 08:36:17 +0800

    Feat：增加对redis集群模式、哨兵模式的支持 (#965)
    * 修复go plugin相关错误
    
    * Feat：增加对redis集群模式、哨兵模式的支持
    
    Co-authored-by: tanxiao <tanxiao@asiainfo.com>

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"github.com/go-redis/redis/v8"
-	"gorm.io/gorm"
+	"strings"
+	"time"
+
 	"github.com/didi/nightingale/v5/src/pkg/ormx"
 	"github.com/didi/nightingale/v5/src/pkg/tls"
+	"github.com/go-redis/redis/v8"
+	"gorm.io/gorm"
 )
 
 type RedisConfig struct {
@@ -17,6 +20,8 @@ type RedisConfig struct {
 	DB       int
 	UseTLS   bool
 	tls.ClientConfig
+	RedisType  string
+	MasterName string
 }
 
 var DB *gorm.DB
@@ -29,26 +34,81 @@ func InitDB(cfg ormx.DBConfig) error {
 	return err
 }
 
-var Redis *redis.Client
+var Redis interface {
+	Del(ctx context.Context, keys ...string) *redis.IntCmd
+	Get(ctx context.Context, key string) *redis.StringCmd
+	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) *redis.StatusCmd
+	HGetAll(ctx context.Context, key string) *redis.StringStringMapCmd
+	HSet(ctx context.Context, key string, values ...interface{}) *redis.IntCmd
+	HDel(ctx context.Context, key string, fields ...string) *redis.IntCmd
+	Close() error
+	Ping(ctx context.Context) *redis.StatusCmd
+	Publish(ctx context.Context, channel string, message interface{}) *redis.IntCmd
+}
 
 func InitRedis(cfg RedisConfig) (func(), error) {
-	redisOptions := &redis.Options{
-		Addr:     cfg.Address,
-		Username: cfg.Username,
-		Password: cfg.Password,
-		DB:       cfg.DB,
-	}
-
-	if cfg.UseTLS {
-		tlsConfig, err := cfg.TLSConfig()
-		if err != nil {
-			fmt.Println("failed to init redis tls config:", err)
-			os.Exit(1)
+	switch cfg.RedisType {
+	case "standalone":
+		redisOptions := &redis.Options{
+			Addr:     cfg.Address,
+			Username: cfg.Username,
+			Password: cfg.Password,
+			DB:       cfg.DB,
 		}
-		redisOptions.TLSConfig = tlsConfig
-	}
 
-	Redis = redis.NewClient(redisOptions)
+		if cfg.UseTLS {
+			tlsConfig, err := cfg.TLSConfig()
+			if err != nil {
+				fmt.Println("failed to init redis tls config:", err)
+				os.Exit(1)
+			}
+			redisOptions.TLSConfig = tlsConfig
+		}
+
+		Redis = redis.NewClient(redisOptions)
+
+	case "cluster":
+		redisOptions := &redis.ClusterOptions{
+			Addrs:    strings.Split(cfg.Address, ","),
+			Username: cfg.Username,
+			Password: cfg.Password,
+		}
+
+		if cfg.UseTLS {
+			tlsConfig, err := cfg.TLSConfig()
+			if err != nil {
+				fmt.Println("failed to init redis tls config:", err)
+				os.Exit(1)
+			}
+			redisOptions.TLSConfig = tlsConfig
+		}
+
+		Redis = redis.NewClusterClient(redisOptions)
+
+	case "sentinel":
+		redisOptions := &redis.FailoverOptions{
+			MasterName:    cfg.MasterName,
+			SentinelAddrs: strings.Split(cfg.Address, ","),
+			Username:      cfg.Username,
+			Password:      cfg.Password,
+			DB:            cfg.DB,
+		}
+
+		if cfg.UseTLS {
+			tlsConfig, err := cfg.TLSConfig()
+			if err != nil {
+				fmt.Println("failed to init redis tls config:", err)
+				os.Exit(1)
+			}
+			redisOptions.TLSConfig = tlsConfig
+		}
+
+		Redis = redis.NewFailoverClient(redisOptions)
+
+	default:
+		fmt.Println("failed to init redis , redis type is illegal:", cfg.RedisType)
+		os.Exit(1)
+	}
 
 	err := Redis.Ping(context.Background()).Err()
 	if err != nil {
