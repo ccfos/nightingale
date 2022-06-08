0581e02cf3a159ee14c142ce6cf93dfb4f83a626
xtan <38320121+tanxiao1990@users.noreply.github.com>
2022-06-08 20:40:45 +0800

    Feat：add common template functions (#976)
    * Feat：增加常用模板函数
    
    * Feat：修改增加模板函数的实现方式
    
    Co-authored-by: tanxiao <tanxiao@asiainfo.com>

--- src/pkg/tplx/tplx.go
+++ src/pkg/tplx/tplx.go
@@ -2,24 +2,23 @@ package tplx
 
 import (
 	"html/template"
-	"time"
+	"regexp"
+	"strings"
 )
 
 var TemplateFuncMap = template.FuncMap{
-	"unescaped":  func(str string) interface{} { return template.HTML(str) },
-	"urlconvert": func(str string) interface{} { return template.URL(str) },
-	"timeformat": func(ts int64, pattern ...string) string {
-		defp := "2006-01-02 15:04:05"
-		if len(pattern) > 0 {
-			defp = pattern[0]
-		}
-		return time.Unix(ts, 0).Format(defp)
-	},
-	"timestamp": func(pattern ...string) string {
-		defp := "2006-01-02 15:04:05"
-		if len(pattern) > 0 {
-			defp = pattern[0]
-		}
-		return time.Now().Format(defp)
-	},
+	"unescaped":           Unescaped,
+	"urlconvert":          Urlconvert,
+	"timeformat":          Timeformat,
+	"timestamp":           Timestamp,
+	"args":                Args,
+	"reReplaceAll":        ReReplaceAll,
+	"match":               regexp.MatchString,
+	"toUpper":             strings.ToUpper,
+	"toLower":             strings.ToLower,
+	"humanize":            Humanize,
+	"humanize1024":        Humanize1024,
+	"humanizeDuration":    HumanizeDuration,
+	"humanizePercentage":  HumanizePercentage,
+	"humanizePercentageH": HumanizePercentageH,
 }
