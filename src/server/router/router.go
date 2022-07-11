94847d9059e7a4d57bdd70dc47a87de73cf07d0b
zheng <bbaobelief@163.com>
2022-07-11 13:06:11 +0800

    get rule node (#1042)

--- src/server/router/router.go
+++ src/server/router/router.go
@@ -92,6 +92,7 @@ func configRoute(r *gin.Engine, version string) {
 	r.GET("/memory/target", targetGet)
 	r.GET("/memory/user", userGet)
 	r.GET("/memory/user-group", userGroupGet)
+	r.GET("/memory/node/alert-rule", alertRuleNodeGet)
 
 	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
