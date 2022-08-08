8bd43afe7acc6daed382cc9e4c6a7a8aedeecddf
ziv <xiaozheng@tuya.com>
2022-08-08 18:37:37 +0800

    feat: support i18n request headerkey

--- src/webapi/config/config.go
+++ src/webapi/config/config.go
@@ -77,6 +77,7 @@ func MustLoad(fpaths ...string) {
 type Config struct {
 	RunMode              string
 	I18N                 string
+	I18NHeaderKey        string
 	AdminRole            string
 	MetricsYamlFile      string
 	BuiltinAlertsDir     string
