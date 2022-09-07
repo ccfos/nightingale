23eb766c14c7021533b6b1b58e9b326bd9e4ef80
xtan <38320121+tanxiao1990@users.noreply.github.com>
2022-09-07 12:24:38 +0800

    feat: alert_subscribe add name and disabled (#1145)
    * feat: alert_subscribe add name and disabled
    
    * feat: alert_subscribe add name and disabled
    
    Co-authored-by: tanxiao <tanxiao@asiainfo.com>

--- src/webapi/router/router_alert_subscribe.go
+++ src/webapi/router/router_alert_subscribe.go
@@ -74,6 +74,8 @@ func alertSubscribePut(c *gin.Context) {
 		fs[i].UpdateBy = username
 		fs[i].UpdateAt = timestamp
 		ginx.Dangerous(fs[i].Update(
+			"name",
+			"disabled",
 			"cluster",
 			"rule_id",
 			"tags",
