7a190b152c71630086d6a3aa20cd943d2429ca1d
Yening Qin <710leo@gmail.com>
2022-11-22 21:53:34 +0800

    feat: add timeseries sample log filter (#1281)
    feat: add timeseries sample log filter

--- src/server/router/router.go
+++ src/server/router/router.go
@@ -101,6 +101,10 @@ func configRoute(r *gin.Engine, version string, reloadFunc func()) {
 
 	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
+	r.GET("/log-sample-filter", logSampleFilterGet)
+	r.POST("/log-sample-filter", logSampleFilterAdd)
+	r.DELETE("/log-sample-filter", logSampleFilterDel)
+
 	service := r.Group("/v1/n9e")
 	service.POST("/event", pushEventToQueue)
 	service.POST("/make-event", makeEvent)
