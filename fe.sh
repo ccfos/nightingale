c366a641a4930c762263553751df6ff90a8eeebb
ning <710leo@gmail.com>
2023-03-10 11:10:57 +0800

    update goreleaser

--- fe.sh
+++ fe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TAG=$(curl -sX GET https://api.github.com/repos/n9e/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
-VERSION=$(echo $TAG | sed 's/v//g')
+VERSION=$(echo $TAG)
 
 curl -o n9e-fe-${VERSION}.tar.gz -L https://github.com/n9e/fe/releases/download/${TAG}/n9e-fe-${VERSION}.tar.gz  
 
