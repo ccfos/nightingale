7833aae0a1d4593f285b5a4f824478c501c957bf
Ulric Qin <ulric.qin@gmail.com>
2023-06-02 08:13:08 +0800

    code refactor

--- fe.sh
+++ fe.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-TAG=$(curl -sX GET https://api.github.com/repos/n9e/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
-VERSION=$(echo $TAG)
+cp -f ./docker/initsql/a-n9e.sql n9e.sql
 
-if ! curl -o n9e-fe-${VERSION}.tar.gz -L https://github.com/n9e/fe/releases/download/${TAG}/n9e-fe-${VERSION}.tar.gz; then
-    echo "failed to download n9e-fe-${VERSION}.tar.gz!"
-    exit 2
+if [ ! -d "./pub" ]; then
+    TAG=$(curl -sX GET https://api.github.com/repos/n9e/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
+
+    if ! curl -o n9e-fe-${TAG}.tar.gz -L https://github.com/n9e/fe/releases/download/${TAG}/n9e-fe-${TAG}.tar.gz; then
+        echo "failed to download n9e-fe-${TAG}.tar.gz!"
+        exit 1
+    fi
+
+    if ! tar zxf n9e-fe-${TAG}.tar.gz; then
+        echo "failed to untar n9e-fe-${TAG}.tar.gz!"
+        exit 2
+    fi
 fi
 
-if ! tar zxvf n9e-fe-${VERSION}.tar.gz; then
-    echo "failed to untar n9e-fe-${VERSION}.tar.gz!"
-    exit 3
-fi
-
-cp ./docker/initsql/a-n9e.sql n9e.sql
 
 # Embed files into a go binary
 if ! $GOPATH/bin/statik -src=./pub -dest=./front; then
