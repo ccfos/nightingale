f6d134b60fcbe6fd3662e31b9a160428c7641472
ning <710leo@gmail.com>
2023-07-12 11:33:08 +0800

    code refactor

--- models/alert_cur_event.go
+++ models/alert_cur_event.go
@@ -60,6 +60,7 @@ type AlertCurEvent struct {
 	NotifyCurNumber          int               `json:"notify_cur_number"`       // notify: current number
 	FirstTriggerTime         int64             `json:"first_trigger_time"`      // 连续告警的首次告警时间
 	ExtraConfig              interface{}       `json:"extra_config" gorm:"-"`
+	Status                   int               `json:"status" gorm:"-"`
 	SubRuleId                int64             `json:"sub_rule_id" gorm:"-"`
 }
 
