baecab7b79bbf430469a2406a0a7d60044e04df8
shardingHe <wangzihe@flashcat.cloud>
2023-08-24 22:45:12 +0800

    auto migrate alerting_engines[engine_cluster],task_record[event_id],chart_share[datasource_id],recording_rule[datasource_ids]

--- models/migrate/migrate.go
+++ models/migrate/migrate.go
@@ -11,10 +11,6 @@ func Migrate(db *gorm.DB) {
 	MigrateEsIndexPatternTable(db)
 }
 
-type RecordingRule struct {
-	QueryConfigs string `gorm:"type:text;not null;column:query_configs"` // query_configs
-}
-
 func MigrateRecordingTable(db *gorm.DB) error {
 	err := db.AutoMigrate(&RecordingRule{})
 	if err != nil {
@@ -39,6 +35,20 @@ func MigrateRecordingTable(db *gorm.DB) error {
 		logger.Errorf("failed to migrate recording rule table: %v", err)
 		return err
 	}
+	if db.Migrator().HasColumn(&AlertingEngines{}, "cluster") {
+		err = db.Migrator().RenameColumn(&AlertingEngines{}, "cluster", "engine_cluster")
+		if err != nil {
+			logger.Errorf("failed to migrate recording rule table: %v", err)
+			return err
+		}
+	}
+
+	err = db.AutoMigrate(TaskRecord{}, ChartShare{})
+	if err != nil {
+		logger.Errorf("failed to migrate recording rule table: %v", err)
+		return err
+	}
+
 	return nil
 }
 
@@ -54,4 +64,20 @@ type AlertSubscribe struct {
 
 type AlertMute struct {
 	Severities string `gorm:"column:severities;type:varchar(32);not null;default:''"`
+}
+
+type RecordingRule struct {
+	QueryConfigs  string `gorm:"type:text;not null;column:query_configs"` // query_configs
+	DatasourceIds string `gorm:"column:datasource_ids;type:varchar(255);default:'';comment:datasource ids"`
+}
+
+type AlertingEngines struct {
+	EngineCluster string `gorm:"column:engine_cluster;type:varchar(128);default:'';comment:n9e engine cluster"`
+}
+
+type ChartShare struct {
+	DatasourceId int64 `gorm:"column:datasource_id;bigint(20);not null;default:0;comment:datasource id"`
+}
+type TaskRecord struct {
+	EventId int64 `gorm:"column:event_id;bigint(20);not null;default:0;comment:event id;index:idx_event_id"`
 }
