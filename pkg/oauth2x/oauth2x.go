267974436a352ff617da795ea71aea57e8b6dbef
shardingHe <wangzihe@flashcat.cloud>
2023-09-08 20:17:13 +0800

    oauth2 add CallbackOutput debug log

--- pkg/oauth2x/oauth2x.go
+++ pkg/oauth2x/oauth2x.go
@@ -195,13 +195,25 @@ func (s *SsoClient) exchangeUser(code string) (*CallbackOutput, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to exchange token: %s", err)
 	}
-
-	userInfo, err := s.getUserInfo(s.UserInfoAddr, oauth2Token.AccessToken, s.TranTokenMethod)
+	var userInfo []byte
+	userInfo, err = s.getUserInfo(s.UserInfoAddr, oauth2Token.AccessToken, s.TranTokenMethod)
 	if err != nil {
 		logger.Errorf("failed to get user info: %s", err)
 		return nil, fmt.Errorf("failed to get user info: %s", err)
 	}
-
+	var data jsoniter.RawMessage
+	var userInfoStr string
+	err = jsoniter.Unmarshal(userInfo, &data)
+	if err != nil {
+		logger.Errorf("failed to get user info(Unmarshal): %s", err)
+		return nil, fmt.Errorf("failed to get user info(Unmarshal): %s", err)
+	}
+	userInfoStr, err = jsoniter.MarshalToString(data)
+	if err != nil {
+		logger.Errorf("failed to get user info(MarshalToString): %s", err)
+		return nil, fmt.Errorf("failed to get user info(MarshalToString): %s", err)
+	}
+	logger.Debugf("get userInfo: %s", userInfoStr)
 	return &CallbackOutput{
 		AccessToken: oauth2Token.AccessToken,
 		Username:    getUserinfoField(userInfo, s.UserinfoIsArray, s.UserinfoPrefix, s.Attributes.Username),
@@ -251,7 +263,7 @@ func (s *SsoClient) getUserInfo(UserInfoAddr, accessToken string, TranTokenMetho
 	body, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		return nil, nil
+		return nil, err
 	}
 	return body, err
 }
