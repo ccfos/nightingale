2119ca115a798910785f534cf19909a0456edd40
shardingHe <wangzihe@flashcat.cloud>
2023-09-19 11:12:11 +0800

    configs form user crud

---
+++ center/router/router_user_variable_config.go
@@ -0,0 +1,71 @@
+package router
+
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/ccfos/nightingale/v6/pkg/secu"
+	"strings"
+
+	"github.com/ccfos/nightingale/v6/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/toolkits/pkg/ginx"
+)
+
+func (rt *Router) userVariableConfigGets(context *gin.Context) {
+	query := strings.TrimSpace(ginx.QueryStr(context, "query", ""))
+	userVariables, err := models.ConfigsGetUserVariable(rt.Ctx, query)
+	ginx.NewRender(context).Data(userVariables, err)
+}
+func (rt *Router) userVariableConfigAdd(context *gin.Context) {
+	var f models.Configs
+	ginx.BindJSON(context, &f)
+	f.Ckey = strings.TrimSpace(f.Ckey)
+	//insert external config. needs to make sure not plaintext for an encrypted type config
+	objs, err := models.ConfigsSelectByCkey(rt.Ctx, f.Ckey)
+	ginx.Dangerous(err)
+	if len(objs) > 0 {
+		ginx.NewRender(context).Message(fmt.Errorf("duplicate ckey found: '%s'", f.Ckey))
+	} else {
+		ginx.NewRender(context).Message(models.ConfigsSetPlus(rt.Ctx, f, &rt.HTTP.RSA))
+	}
+}
+
+func (rt *Router) userVariableConfigPut(context *gin.Context) {
+	var f models.Configs
+	ginx.BindJSON(context, &f)
+	f.Ckey = strings.TrimSpace(f.Ckey)
+	//insert external config. needs to make sure not plaintext for an encrypted type config
+	objs, err := models.ConfigsSelectByCkey(rt.Ctx, f.Ckey)
+	ginx.Dangerous(err)
+	if len(objs) == 0 {
+		ginx.NewRender(context).Message(fmt.Errorf("not found ckey: '%s'", f.Ckey))
+	} else {
+		//update external config. needs to make sure not plaintext for an encrypted type config
+		//updating with struct it will update all fields ("cval", "note", "external", "encrypted"),not non-zero fields.
+		ginx.NewRender(context).Message(models.ConfigsSetPlus(rt.Ctx, f, &rt.HTTP.RSA))
+	}
+}
+
+func (rt *Router) userVariableConfigDel(context *gin.Context) {
+	id := ginx.UrlParamInt64(context, "id")
+	configs, err := models.ConfigGet(rt.Ctx, id)
+	ginx.Dangerous(err)
+
+	if configs != nil && configs.External == models.Config_External {
+		ginx.NewRender(context).Message(models.ConfigsDel(rt.Ctx, []int64{id}))
+	} else {
+		ginx.NewRender(context).Message(nil)
+	}
+}
+
+//todo for test
+func (rt *Router) userVariableEncrypted(context *gin.Context) {
+	publicKey := ginx.QueryStr(context, "public_key")
+	decodeCipher, errKey := base64.StdEncoding.DecodeString(publicKey)
+	ginx.Dangerous(errKey)
+	// got a plaintext need to encrypting
+	ciphertext, err := secu.EncryptValue(ginx.QueryStr(context, "plaintext"), decodeCipher)
+	ginx.NewRender(context).Data(ciphertext, err)
+
+}
