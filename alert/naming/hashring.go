b4fd4f108759f564a094f0f1a398757630797b68
Ulric Qin <ulric.qin@gmail.com>
2023-09-27 09:59:49 +0800

    code refactor

--- alert/naming/hashring.go
+++ alert/naming/hashring.go
@@ -54,7 +54,7 @@ func (chr *DatasourceHashRingType) IsHit(datasourceId int64, pk string, currentN
 	node, err := chr.GetNode(datasourceId, pk)
 	if err != nil {
 		if !errors.Is(err, consistent.ErrEmptyCircle) {
-			logger.Debugf("rule id:%s is not work, datasource id:%d failed to get node from hashring:%v", pk, datasourceId, err)
+			logger.Errorf("rule id:%s is not work, datasource id:%d failed to get node from hashring:%v", pk, datasourceId, err)
 		}
 		return false
 	}
