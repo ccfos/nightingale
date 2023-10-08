3837d8884dafdefe9ff3cab6781deac172da0ac8
shardingHe <wangzihe@flashcat.cloud>
2023-10-08 18:18:42 +0800

    update mock struct

--- pkg/poster/post_test.go
+++ pkg/poster/post_test.go
@@ -10,12 +10,6 @@ import (
 	"github.com/ccfos/nightingale/v6/pkg/ctx"
 )
 
-type HeartbeatInfo struct {
-	Instance      string `json:"instance"`
-	EngineCluster string `json:"engine_cluster"`
-	DatasourceId  int64  `json:"datasource_id"`
-}
-
 func TestPostByUrls(t *testing.T) {
 	type args struct {
 		ctx  *ctx.Context
@@ -23,10 +17,12 @@ func TestPostByUrls(t *testing.T) {
 		v    interface{}
 	}
 
-	info := HeartbeatInfo{
-		Instance:      "instance",
-		EngineCluster: "cluster",
-		DatasourceId:  888,
+	info := struct {
+		a string
+		b int
+	}{
+		a: "aaa",
+		b: 888,
 	}
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		response := DataResponse[interface{}]{Dat: "", Err: ""}
@@ -53,24 +49,6 @@ func TestPostByUrls(t *testing.T) {
 	}
 }
 
-type AlertCurEvent_Temp struct {
-	Id              int64  `json:"id" gorm:"primaryKey"`
-	Cate            string `json:"cate"`
-	Cluster         string `json:"cluster"`
-	DatasourceId    int64  `json:"datasource_id"`
-	GroupId         int64  `json:"group_id"`   // busi group id
-	GroupName       string `json:"group_name"` // busi group name
-	Hash            string `json:"hash"`       // rule_id + vector_key
-	RuleId          int64  `json:"rule_id"`
-	RuleName        string `json:"rule_name"`
-	RuleNote        string `json:"rule_note"`
-	RuleProd        string `json:"rule_prod"`
-	RuleAlgo        string `json:"rule_algo"`
-	Severity        int    `json:"severity"`
-	PromForDuration int    `json:"prom_for_duration"`
-	PromQl          string `json:"prom_ql"`
-}
-
 func TestPostByUrlsWithResp(t *testing.T) {
 	type args struct {
 		ctx  *ctx.Context
@@ -81,21 +59,23 @@ func TestPostByUrlsWithResp(t *testing.T) {
 		name string
 		args args
 	}
+	info := struct {
+		a string
+		b int
+	}{
+		a: "aaa",
+		b: 888,
+	}
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		response := DataResponse[int64]{Dat: 123, Err: ""}
 		json.NewEncoder(w).Encode(response)
 	}))
 	defer server.Close()
-	tests := []testCase[int64]{{"a-resp", args{
-		ctx: &ctx.Context{
-			CenterApi: conf.CenterApi{
-				Addrs: []string{server.URL},
-			}},
-		path: "/v1/n9e/event-persist",
-		v:    AlertCurEvent_Temp{PromQl: "PromQl", Cate: "Cate"},
-	},
-	},
-	}
+	tests := []testCase[int64]{{"a-resp", args{ctx: &ctx.Context{
+		CenterApi: conf.CenterApi{
+			Addrs: []string{server.URL},
+		}}, path: "/v1/n9e/event-persist",
+		v: info}}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			gotT, err := PostByUrlsWithResp[int64](tt.args.ctx, tt.args.path, tt.args.v)
