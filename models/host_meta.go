ba0507838bcf2c6eba1b4ad9a6175531bafe7908
ning <710leo@gmail.com>
2023-10-17 17:05:25 +0800

    refactor: heartbeat api

--- models/host_meta.go
+++ models/host_meta.go
@@ -3,17 +3,18 @@ package models
 import "encoding/json"
 
 type HostMeta struct {
-	AgentVersion string  `json:"agent_version"`
-	OS           string  `json:"os"`
-	Arch         string  `json:"arch"`
-	Hostname     string  `json:"hostname"`
-	CpuNum       int     `json:"cpu_num"`
-	CpuUtil      float64 `json:"cpu_util"`
-	MemUtil      float64 `json:"mem_util"`
-	Offset       int64   `json:"offset"`
-	UnixTime     int64   `json:"unixtime"`
-	RemoteAddr   string  `json:"remote_addr"`
-	HostIp       string  `json:"host_ip"`
+	AgentVersion string            `json:"agent_version"`
+	OS           string            `json:"os"`
+	Arch         string            `json:"arch"`
+	Hostname     string            `json:"hostname"`
+	CpuNum       int               `json:"cpu_num"`
+	CpuUtil      float64           `json:"cpu_util"`
+	MemUtil      float64           `json:"mem_util"`
+	Offset       int64             `json:"offset"`
+	UnixTime     int64             `json:"unixtime"`
+	RemoteAddr   string            `json:"remote_addr"`
+	HostIp       string            `json:"host_ip"`
+	GlobalLabels map[string]string `json:"global_labels"`
 }
 
 func (h HostMeta) MarshalBinary() ([]byte, error) {
