673c7d8f307d6e09a4a487b5ecafe7cc58f2ed8a
shardingHe <wangzihe@flashcat.cloud>
2023-11-28 11:54:18 +0800

    optimize gorm logger

--- pkg/ormx/ormx.go
+++ pkg/ormx/ormx.go
@@ -2,10 +2,10 @@ package ormx
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 	"time"
 
-	"github.com/ccfos/nightingale/v6/pkg/logx"
 	tklog "github.com/toolkits/pkg/logger"
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
@@ -23,6 +23,50 @@ type DBConfig struct {
 	MaxOpenConns int
 	MaxIdleConns int
 	TablePrefix  string
+}
+
+var t_log = logger.New(
+	&TKitLogger{tklog.GetLogger()},
+	logger.Config{
+		SlowThreshold:             2 * time.Second,
+		LogLevel:                  logger.Warn,
+		IgnoreRecordNotFoundError: false,
+		Colorful:                  true,
+	},
+)
+var t_log_level map[string]logger.LogLevel
+
+func init() {
+	t_log_level = make(map[string]logger.LogLevel, 8)
+	v := reflect.ValueOf(t_log).Elem()
+	t_log_level[v.FieldByName("infoStr").String()] = logger.Info
+	t_log_level[v.FieldByName("warnStr").String()] = logger.Warn
+	t_log_level[v.FieldByName("errStr").String()] = logger.Error
+	t_log_level[v.FieldByName("traceStr").String()] = logger.Info
+	t_log_level[v.FieldByName("traceWarnStr").String()] = logger.Warn
+	t_log_level[v.FieldByName("traceErrStr").String()] = logger.Error
+
+}
+
+type TKitLogger struct {
+	logger *tklog.Logger
+}
+
+func (l *TKitLogger) Printf(s string, i ...interface{}) {
+	level, ok := t_log_level[s]
+	if !ok {
+		l.logger.Debugf(s, i...)
+	}
+	switch level {
+	case logger.Info:
+		l.logger.Infof(s, i...)
+	case logger.Warn:
+		l.logger.Warningf(s, i...)
+	case logger.Error:
+		l.logger.Errorf(s, i...)
+	default:
+		l.logger.Debugf(s, i...)
+	}
 }
 
 // New Create gorm.DB instance
@@ -43,17 +87,7 @@ func New(c DBConfig) (*gorm.DB, error) {
 			TablePrefix:   c.TablePrefix,
 			SingularTable: true,
 		},
-		Logger: &logx.GORMLogger{
-			Logger:        tklog.GetLogger(),
-			LogLevel:      logger.Warn,
-			SlowThreshold: 2 * time.Second,
-			InfoStr:       logx.InfoStr,
-			WarnStr:       logx.WarnStr,
-			ErrStr:        logx.ErrStr,
-			TraceStr:      logx.TraceStr,
-			TraceWarnStr:  logx.TraceWarnStr,
-			TraceErrStr:   logx.TraceErrStr,
-		},
+		Logger: t_log,
 	}
 
 	db, err := gorm.Open(dialector, gconfig)
