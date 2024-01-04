741ab9415018e4014c61da98add0911ceeb997aa
Deke Wang <94156972+wdkcc@users.noreply.github.com>
2024-01-04 19:23:05 +0800

    docs: add docker-compose files for logs processing (#1819)
    * add: docker-compose files for logs processing
    
    * update: set restart:always
    
    * fix: compose-host-network-metric-log
    
    * update: regularize
    
    * fix: categraf will panic when it starts
    
    * fix: add WAIT_HOSTS of kafka for categraf and set es index=n9e-y.m.d
    
    ---------
    
    Co-authored-by: wdk <wdk_cc@163.com>

---
+++ docker/compose-host-network-metric-log/etc-nightingale/script/notify.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import sys
+import json
+
+class Sender(object):
+    @classmethod
+    def send_email(cls, payload):
+        # already done in go code
+        pass
+
+    @classmethod
+    def send_wecom(cls, payload):
+        # already done in go code
+        pass
+
+    @classmethod
+    def send_dingtalk(cls, payload):
+        # already done in go code
+        pass
+
+    @classmethod
+    def send_feishu(cls, payload):
+        # already done in go code
+        pass
+
+    @classmethod
+    def send_mm(cls, payload):
+        # already done in go code
+        pass
+
+    @classmethod
+    def send_sms(cls, payload):
+        users = payload.get('event').get("notify_users_obj")
+        phones = {}
+        for u in users:
+            if u.get("phone"):
+                phones[u.get("phone")] = 1
+        if phones:
+            print("send_sms not implemented, phones: {}".format(phones.keys()))
+
+    @classmethod
+    def send_voice(cls, payload):
+        users = payload.get('event').get("notify_users_obj")
+        phones = {}
+        for u in users:
+            if u.get("phone"):
+                phones[u.get("phone")] = 1
+        if phones:
+            print("send_voice not implemented, phones: {}".format(phones.keys()))
+
+def main():
+    payload = json.load(sys.stdin)
+    with open(".payload", 'w') as f:
+        f.write(json.dumps(payload, indent=4))
+    for ch in payload.get('event').get('notify_channels'):
+        send_func_name = "send_{}".format(ch.strip())
+        if not hasattr(Sender, send_func_name):
+            print("function: {} not found", send_func_name)
+            continue
+        send_func = getattr(Sender, send_func_name)
+        send_func(payload)
+
+def hello():
+    print("hello nightingale")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        main()
+    elif sys.argv[1] == "hello":
+        hello()
+    else:
+        print("I am confused")
