8efa451d75491be82fc8283cbf04ad128b573944
ning <710leo@gmail.com>
2024-04-03 16:52:07 +0800

    code refactor

--- models/migrate/migrate.go
+++ models/migrate/migrate.go
@@ -26,7 +26,7 @@ func MigrateIbexTables(db *gorm.DB) {
 	}
 
 	for i := 0; i < 100; i++ {
-		tableName := fmt.Sprintf("task_host_%02d", i)
+		tableName := fmt.Sprintf("task_host_%d", i)
 		err := db.Table(tableName).AutoMigrate(&imodels.TaskHost{})
 		if err != nil {
 			logger.Errorf("failed to migrate table:%s %v", tableName, err)
