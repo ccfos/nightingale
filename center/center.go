fb1cc4868e2128af23707fe47938d9b186d5f774
shardingHe <125482062+shardingHe@users.noreply.github.com>
2024-05-08 14:10:25 +0800

    feat: add user variable for sso(decrypted). (#1936)
    * add user variable for sso(decrypted).
    ---------
    
    Co-authored-by: shardingHe <wangzihe@flashcat.cloud>

--- center/center.go
+++ center/center.go
@@ -80,8 +80,6 @@ func Initialize(configDir string, cryptoKey string) (func(), error) {
 	syncStats := memsto.NewSyncStats()
 	alertStats := astats.NewSyncStats()
 
-	sso := sso.Init(config.Center, ctx)
-
 	configCache := memsto.NewConfigCache(ctx, syncStats, config.HTTP.RSA.RSAPrivateKey, config.HTTP.RSA.RSAPassWord)
 	busiGroupCache := memsto.NewBusiGroupCache(ctx, syncStats)
 	targetCache := memsto.NewTargetCache(ctx, syncStats, redis)
@@ -93,6 +91,7 @@ func Initialize(configDir string, cryptoKey string) (func(), error) {
 	userGroupCache := memsto.NewUserGroupCache(ctx, syncStats)
 	taskTplCache := memsto.NewTaskTplCache(ctx)
 
+	sso := sso.Init(config.Center, ctx, configCache)
 	promClients := prom.NewPromClient(ctx)
 	tdengineClients := tdengine.NewTdengineClient(ctx, config.Alert.Heartbeat)
 
