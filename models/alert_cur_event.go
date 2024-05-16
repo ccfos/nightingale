07436a5e0d8af19b2d68fc7b1ca9c2ceef21f1f2
ning <710leo@gmail.com>
2024-05-16 11:15:18 +0800

    refactor: change event order

--- models/alert_cur_event.go
+++ models/alert_cur_event.go
@@ -496,7 +496,7 @@ func AlertCurEventGetByIds(ctx *ctx.Context, ids []int64) ([]*AlertCurEvent, err
 		return lst, nil
 	}
 
-	err := DB(ctx).Where("id in ?", ids).Order("id desc").Find(&lst).Error
+	err := DB(ctx).Where("id in ?", ids).Order("trigger_time desc").Find(&lst).Error
 	if err == nil {
 		for i := 0; i < len(lst); i++ {
 			lst[i].DB2FE()
