08749d3575b58271a68074c048abc01e749bcac6
chenyunda218 <chenyunda218@gmail.com>
2024-07-10 17:12:15 +0800

    feat: check webhook if nil

--- alert/sender/webhook.go
+++ alert/sender/webhook.go
@@ -47,12 +47,14 @@ func sendWebhook(webhook *models.Webhook, event *models.AlertCurEvent, stats *as
 			req.Header.Set(conf.Headers[i], conf.Headers[i+1])
 		}
 	}
-
-	// todo add skip verify
+	insecureSkipVerify := false
+	if webhook != nil {
+		insecureSkipVerify = webhook.SkipVerify
+	}
 	client := http.Client{
 		Timeout: time.Duration(conf.Timeout) * time.Second,
 		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: webhook.SkipVerify},
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: insecureSkipVerify},
 		},
 	}
 
