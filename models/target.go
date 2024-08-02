d82d55a87f163f579a2676279c085723f0b171fe
chenyunda218 <chenyunda218@gmail.com>
2024-08-02 12:45:27 +0800

    fixup

--- models/target.go
+++ models/target.go
@@ -164,6 +164,9 @@ func BuildTargetWhereWithOffset(offset int) BuildTargetWhereOption {
 
 func BuildTargetWhereWithIdents(idents []string) BuildTargetWhereOption {
 	return func(session *gorm.DB) *gorm.DB {
+		if len(idents) == 0 {
+			return session
+		}
 		return session.Where("ident in ?", idents)
 	}
 }
@@ -388,7 +391,7 @@ func TargetGetTags(ctx *ctx.Context, idents []string) ([]string, error) {
 }
 
 func (t *Target) AddTags(ctx *ctx.Context, tags []string) error {
-	for i := 0; i < len(tags); i++ {
+	for i := range tags {
 		if !strings.Contains(t.Tags, tags[i]+" ") {
 			t.Tags += tags[i] + " "
 		}
