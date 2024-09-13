49487709b54f86b18a63c3e73b709ab40b2c3777
wenbo <a1027758873@qq.com>
2024-09-14 00:19:34 +0800

    fix

--- models/alert_rule.go
+++ models/alert_rule.go
@@ -177,9 +177,10 @@ func GetHostsQuery(queries []HostQuery) []map[string]interface{} {
 		case "group_ids":
 			ids := ParseInt64(q.Values)
 			if q.Op == "==" {
-				m["group_id in (?)"] = ids
+				m["target_busi_group.group_id in (?)"] = ids
 			} else {
-				m["group_id not in (?)"] = ids
+				m["target.ident not in (select target_ident "+
+					"from target_busi_group where group_id = ?)"] = ids
 			}
 		case "tags":
 			lst := []string{}
