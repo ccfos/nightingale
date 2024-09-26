d190509fda519655ce9e8d52f7579793187ba50f
red <erickbin@stu.pku.edu.cn>
2024-09-26 19:02:59 +0800

    alert recover

--- alert/common/conv.go
+++ alert/common/conv.go
@@ -5,21 +5,18 @@ import (
 	"math"
 	"strings"
 
-	"github.com/ccfos/nightingale/v6/models"
-
 	"github.com/prometheus/common/model"
 )
 
 type AnomalyPoint struct {
-	Key           string               `json:"key"`
-	Labels        model.Metric         `json:"labels"`
-	Timestamp     int64                `json:"timestamp"`
-	Value         float64              `json:"value"`
-	Severity      int                  `json:"severity"`
-	Triggered     bool                 `json:"triggered"`
-	Query         string               `json:"query"`
-	Values        string               `json:"values"`
-	RecoverConfig models.RecoverConfig `json:"recover_config"`
+	Key       string       `json:"key"`
+	Labels    model.Metric `json:"labels"`
+	Timestamp int64        `json:"timestamp"`
+	Value     float64      `json:"value"`
+	Severity  int          `json:"severity"`
+	Triggered bool         `json:"triggered"`
+	Query     string       `json:"query"`
+	Values    string       `json:"values"`
 }
 
 func NewAnomalyPoint(key string, labels map[string]string, ts int64, value float64, severity int) AnomalyPoint {
