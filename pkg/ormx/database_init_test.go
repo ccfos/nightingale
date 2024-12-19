40cf877f1795d11b7c096028622e6b09bcb623cd
CRISPpp <78430796+CRISPpp@users.noreply.github.com>
2024-12-19 15:44:21 +0800

    fix:  postgres init bug (#2370)

--- pkg/ormx/database_init_test.go
+++ pkg/ormx/database_init_test.go
@@ -1,6 +1,7 @@
 package ormx
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,6 +10,43 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+func TestCheckPostgresDatabaseExist(t *testing.T) {
+	tests := []struct {
+		name   string
+		config DBConfig
+	}{
+		{
+			name: "MySQL",
+			config: DBConfig{
+				DBType: "mysql",
+				DSN:    "root:1234@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local&allowNativePasswords=true",
+			},
+		},
+		{
+			name: "Postgres",
+			config: DBConfig{
+				DBType: "postgres",
+				DSN:    "host=127.0.0.1 port=5432 user=root dbname=n9e_v6 password=1234 sslmode=disable",
+			},
+		},
+		{
+			name: "SQLite",
+			config: DBConfig{
+				DBType: "sqlite",
+				DSN:    "./test.db",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			exist, err := checkPostgresDatabaseExist(tt.config)
+			fmt.Printf("exitst: %v", exist)
+			assert.NoError(t, err)
+		})
+	}
+}
 
 func TestDataBaseInit(t *testing.T) {
 	tests := []struct {
