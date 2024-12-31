6654b25a7b0eb020f9500c6a8df165196c2ae228
CRISPpp <2715554797@qq.com>
2024-12-31 06:40:18 +0000

    feat: update

--- alert/router/router_event.go
+++ alert/router/router_event.go
@@ -41,7 +41,7 @@ func (rt *Router) pushEventToQueue(c *gin.Context) {
 		event.TagsMap[arr[0]] = arr[1]
 	}
 	hit, muteId :=  mute.EventMuteStrategy(event, rt.AlertMuteCache)
-	if hit && muteId != -1{
+	if hit && muteId != 0{
 		logger.Infof("event_muted: rule_id=%d %s", event.RuleId, event.Hash)
 		ginx.NewRender(c).Message(nil)
 		return
