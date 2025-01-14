562da5a73fbc9fd04c26a974ad7c9c76e119ade7
Yening Qin <710leo@gmail.com>
2025-01-14 12:08:03 +0800

    refactor: data recv and push to queue (#2427)

--- pushgw/router/router_openfalcon.go
+++ pushgw/router/router_openfalcon.go
@@ -207,9 +207,14 @@ func (rt *Router) falconPush(c *gin.Context) {
 		}
 
 		if ident != "" {
-			rt.ForwardByIdent(c.ClientIP(), ident, pt)
+			err = rt.ForwardByIdent(c.ClientIP(), ident, pt)
 		} else {
-			rt.ForwardByMetric(c.ClientIP(), arr[i].Metric, pt)
+			err = rt.ForwardByMetric(c.ClientIP(), arr[i].Metric, pt)
+		}
+
+		if err != nil {
+			c.String(rt.Pushgw.WriterOpt.OverLimitStatusCode, err.Error())
+			return
 		}
 
 		succ++
