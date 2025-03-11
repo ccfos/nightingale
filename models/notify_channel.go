6b6e6378aadf8f3cff52a63392203ffc248edec7
smx_Moragn <1048492656@qq.com>
2025-03-11 21:54:51 +1100

    fix token name

--- models/notify_channel.go
+++ models/notify_channel.go
@@ -1066,7 +1066,7 @@ var NotiChMap = []*NotifyChannelConfig{
 		RequestConfig: &RequestConfig{
 			HTTPRequestConfig: &HTTPRequestConfig{
 				URL:    "{{$params.mattermost_url}}/api/v4/posts",
-				Method: "POST", Headers: map[string]string{"Content-Type": "application/json", "Authorization": "Bearer <you MattermostBot bot token>"},
+				Method: "POST", Headers: map[string]string{"Content-Type": "application/json", "Authorization": "Bearer <you Mattermost bot token>"},
 				Timeout: 10000, Concurrency: 5, RetryTimes: 3, RetryInterval: 100,
 				Request: RequestDetail{
 					Body: `{"channel_id": "{{$params.channel_id}}", "message":  "{{$tpl.content}}"}`,
