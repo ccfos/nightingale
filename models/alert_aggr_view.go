49cda8b58a5f4c423fea91e5dd5cd90c54d8ddc6
ulricqin <ulric.qin@gmail.com>
2025-05-22 15:08:01 +0800

    modify alerting aggr verify rules (#2694)

--- models/alert_aggr_view.go
+++ models/alert_aggr_view.go
@@ -38,33 +38,35 @@ func (v *AlertAggrView) Verify() error {
 		return errors.New("rule is blank")
 	}
 
-	var validFields = []string{
-		"cluster",
-		"group_id",
-		"group_name",
-		"rule_id",
-		"rule_name",
-		"severity",
-		"runbook_url",
-		"target_ident",
-		"target_note",
-	}
-
-	arr := strings.Split(v.Rule, "::")
-	for i := 0; i < len(arr); i++ {
-		pair := strings.Split(arr[i], ":")
-		if len(pair) != 2 {
-			return errors.New("rule invalid")
+	if !strings.Contains(v.Rule, "{{") {
+		var validFields = []string{
+			"cluster",
+			"group_id",
+			"group_name",
+			"rule_id",
+			"rule_name",
+			"severity",
+			"runbook_url",
+			"target_ident",
+			"target_note",
 		}
 
-		if !(pair[0] == "field" || pair[0] == "tagkey") {
-			return errors.New("rule invalid")
-		}
+		arr := strings.Split(v.Rule, "::")
+		for i := 0; i < len(arr); i++ {
+			pair := strings.Split(arr[i], ":")
+			if len(pair) != 2 {
+				return errors.New("rule invalid")
+			}
 
-		if pair[0] == "field" {
-			// 只支持有限的field
-			if !slice.ContainsString(validFields, pair[1]) {
-				return fmt.Errorf("unsupported field: %s", pair[1])
+			if !(pair[0] == "field" || pair[0] == "tagkey") {
+				return errors.New("rule invalid")
+			}
+
+			if pair[0] == "field" {
+				// 只支持有限的field
+				if !slice.ContainsString(validFields, pair[1]) {
+					return fmt.Errorf("unsupported field: %s", pair[1])
+				}
 			}
 		}
 	}
