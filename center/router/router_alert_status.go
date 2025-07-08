eff72ce440fda01d9092e6255334c9cabae8c8fa
Haobo Zhang <43698160+haobo8@users.noreply.github.com>
2025-07-08 17:35:44 +0800

    update router_alert_status

--- center/router/router_alert_status.go
+++ center/router/router_alert_status.go
@@ -27,41 +27,75 @@ func (rt *Router) QueryAlertStatus(c *gin.Context) {
 
 	DefaultPromDatasourceId := atomic.LoadInt64(&dscache.PromDefaultDatasourceId)
 	if DefaultPromDatasourceId == 0 {
-		ginx.NewRender(c).Message("datasource id is 0")
+		ginx.NewRender(c).Data(map[string]interface{}{
+			"value":     nil,
+			"timestamp": nil,
+		}, nil)
 		return
 	}
 
 	readerClient := rt.PromClients.GetCli(DefaultPromDatasourceId)
 	if readerClient == nil {
-		ginx.NewRender(c).Message(fmt.Sprintf("prometheus client not found for datasource id: %d", DefaultPromDatasourceId))
+		ginx.NewRender(c).Data(map[string]interface{}{
+			"value":     nil,
+			"timestamp": nil,
+		}, nil)
 		return
 	}
 
 	// 构建 PromQL 查询语句
-	metricName := fmt.Sprintf("n9e_alert_rule_%d_status_value", query.RuleId)
-	promql := metricName
+	valueMetricName := fmt.Sprintf("n9e_alert_rule_%d_status_value", query.RuleId)
+	timestampMetricName := fmt.Sprintf("n9e_alert_rule_%d_status_timestamp", query.RuleId)
+	valuePromql := valueMetricName
+	timestampPromql := timestampMetricName
 
 	// 如果有标签过滤，添加到查询中
 	if query.Labels != "" {
-		promql = fmt.Sprintf("%s{%s}", metricName, query.Labels)
+		valuePromql = fmt.Sprintf("%s{%s}", valueMetricName, query.Labels)
+		timestampPromql = fmt.Sprintf("%s{%s}", timestampMetricName, query.Labels)
 	}
 
-	logger.Infof("Querying alert status: %s, time: %v - %v", promql, query.StartTime, query.EndTime)
+	logger.Infof("Querying alert status: %s & %s, time: %v - %v", valuePromql, timestampPromql, query.StartTime, query.EndTime)
 
 	// 使用当前时间进行查询（获取最新状态）
-	value, warnings, err := readerClient.QueryRange(context.Background(), promql, promsdk.Range{
+	value, warnings, err := readerClient.QueryRange(context.Background(), valuePromql, promsdk.Range{
 		Start: time.Unix(query.StartTime, 0),
 		End:   time.Unix(query.EndTime, 0),
 		Step:  time.Minute,
 	})
 
 	if err != nil {
-		ginx.NewRender(c).Message(fmt.Sprintf("failed to query prometheus: %v with query: %v", err, promql))
+		ginx.NewRender(c).Data(map[string]interface{}{
+			"value":     nil,
+			"timestamp": nil,
+		}, nil)
 		return
 	}
 
 	if len(warnings) > 0 {
-		logger.Warningf("Query warnings: %v", warnings)
+		logger.Warningf("Query warnings: %v with query: %s", warnings, valuePromql)
 	}
-	ginx.NewRender(c).Data(value, nil)
+
+	timestamp, warnings, err := readerClient.QueryRange(context.Background(), timestampPromql, promsdk.Range{
+		Start: time.Unix(query.StartTime, 0),
+		End:   time.Unix(query.EndTime, 0),
+		Step:  time.Minute,
+	})
+
+	if err != nil {
+		ginx.NewRender(c).Data(map[string]interface{}{
+			"value":     nil,
+			"timestamp": nil,
+		}, nil)
+		return
+	}
+
+	if len(warnings) > 0 {
+		logger.Warningf("Query warnings: %v with query: %s & %s", warnings, valuePromql, timestampPromql)
+	}
+
+	ginx.NewRender(c).Data(map[string]interface{}{
+		"value":     value,
+		"timestamp": timestamp,
+	}, nil)
 }
