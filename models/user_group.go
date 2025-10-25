19d76824d91e5a270114c926c0cd1e7fa0255074
ning <710leo@gmail.com>
2025-10-25 16:45:31 +0800

    update user group api

--- models/user_group.go
+++ models/user_group.go
@@ -12,15 +12,16 @@ import (
 )
 
 type UserGroup struct {
-	Id       int64   `json:"id" gorm:"primaryKey"`
-	Name     string  `json:"name"`
-	Note     string  `json:"note"`
-	CreateAt int64   `json:"create_at"`
-	CreateBy string  `json:"create_by"`
-	UpdateAt int64   `json:"update_at"`
-	UpdateBy string  `json:"update_by"`
-	UserIds  []int64 `json:"-" gorm:"-"`
-	Users    []User  `json:"users" gorm:"-"`
+	Id            int64           `json:"id" gorm:"primaryKey"`
+	Name          string          `json:"name"`
+	Note          string          `json:"note"`
+	CreateAt      int64           `json:"create_at"`
+	CreateBy      string          `json:"create_by"`
+	UpdateAt      int64           `json:"update_at"`
+	UpdateBy      string          `json:"update_by"`
+	UserIds       []int64         `json:"-" gorm:"-"`
+	Users         []User          `json:"users" gorm:"-"`
+	BusiGroupsRes []*BusiGroupRes `json:"busi_groups" gorm:"-"`
 }
 
 func (ug *UserGroup) TableName() string {
