129d0723d367fdb7a26fb338b686bc439e7c353a
snowykami <snowykami@outlook.com>
2025-11-19 15:26:16 +0800

    feat: add target_group match

--- alert/common/key.go
+++ alert/common/key.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -14,6 +15,12 @@ func RuleKey(datasourceId, id int64) string {
 func MatchTags(eventTagsMap map[string]string, itags []models.TagFilter) bool {
 	for _, filter := range itags {
 		value, has := eventTagsMap[filter.Key]
+		// 当匹配为target_group时，特殊处理
+		if filter.Key == "target_group" {
+			if v, ok := eventTagsMap["target"]; ok {
+				return targetGroupMatch(v, filter)
+			}
+		}
 		if !has {
 			return false
 		}
@@ -51,4 +58,52 @@ func matchTag(value string, filter models.TagFilter) bool {
 	}
 	// unexpected func
 	return false
+}
+
+// targetGroupMatch 处理 target_group 的特殊匹配逻辑
+func targetGroupMatch(value string, filter models.TagFilter) bool {
+	var valueMap map[string]interface{}
+	if err := json.Unmarshal([]byte(value), &valueMap); err != nil {
+		return false
+	}
+	filterValueMap, ok := filter.Value.(map[string]interface{})
+	if !ok {
+		return false
+	}
+
+	switch filter.Func {
+	case "in":
+		groupIds, ok := valueMap["group_ids"].([]interface{})
+		if !ok {
+			return false
+		}
+
+		filterGroupIds, ok := filterValueMap["ids"].([]interface{})
+		if !ok {
+			return false
+		}
+		// 只要 groupIds 中有一个在 filterGroupIds 中出现，就返回 true
+		for _, gid := range groupIds {
+			for _, fgid := range filterGroupIds {
+				if fmt.Sprintf("%v", gid) == fmt.Sprintf("%v", fgid) {
+					return true
+				}
+			}
+		}
+		return false
+	case "=~":
+		// 正则满足一个就返回 true
+		groupNames, ok := valueMap["group_names"].([]interface{})
+		if !ok {
+			return false
+		}
+		for _, gname := range groupNames {
+			if filter.Regexp.MatchString(fmt.Sprintf("%v", gname)) {
+				return true
+			}
+		}
+		return false
+	default:
+		return false
+	}
 }
