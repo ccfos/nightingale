8ff0c1eeb970a9596543e72192efaf2d12dfc45f
ning <710leo@gmail.com>
2025-12-17 18:05:18 +0800

    fix: webhook connection leak

--- models/notify_config.go
+++ models/notify_config.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/toolkits/pkg/str"
 )
@@ -30,6 +31,8 @@ type Webhook struct {
 	RetryCount    int               `json:"retry_count"`
 	RetryInterval int               `json:"retry_interval"`
 	Batch         int               `json:"batch"`
+
+	Client *http.Client `json:"-"`
 }
 
 func (w *Webhook) Hash() string {
